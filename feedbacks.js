5710bff8ca40fad693c08f701486b2ca21d2a0f2
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-08-13 15:24:17 +0300

    Update feedbacks.js

--- feedbacks.js
+++ feedbacks.js
@@ -1,33 +1,15 @@
-const { combineRgb } = require('@companion-module/base')
-
-module.exports = async function (self) {
-	self.setFeedbackDefinitions({
-		ChannelState: {
-			name: 'Example Feedback',
-			type: 'boolean',
-			label: 'Channel State',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-				color: combineRgb(0, 0, 0),
-			},
-			options: [
-				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 10,
-				},
-			],
-			callback: (feedback) => {
-				console.log('Hello world!', feedback.options.num)
-				if (feedback.options.num > 5) {
-					return true
-				} else {
-					return false
-				}
-			},
-		},
-	})
+// feedbacks.js
+export function getFeedbacks(self) {
+  return {
+    pvwLayerCountEquals: {
+      type: 'boolean',
+      name: 'PVW layer count equals',
+      description: 'True when PVW layer count == value',
+      options: [{ type: 'number', id: 'count', label: 'Count', default: 1, min: 0, max: 20 }],
+      callback: (feedback) => {
+        const cnt = self.getPVWLayers().length
+        return cnt === Number(feedback.options.count)
+      },
+    },
+  }
 }

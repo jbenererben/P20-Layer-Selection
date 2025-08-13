9154441d127ccdf80690a388922ca284e9d6e6df
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-08-13 15:23:39 +0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,20 +1,49 @@
-module.exports = function (self) {
-	self.setActionDefinitions({
-		sample_action: {
-			name: 'My First Action',
-			options: [
-				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 100,
-				},
-			],
-			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
-			},
-		},
-	})
+// actions.js
+export function getActions(self) {
+  return {
+    selectScreen: {
+      name: 'Select screen (context)',
+      options: [
+        {
+          type: 'dropdown',
+          id: 'screenId',
+          label: 'Screen',
+          choices: () => self.getScreens().map((s) => ({ id: s.id, label: s.name })),
+          default: () => self.getScreens()[0]?.id,
+        },
+      ],
+      callback: async (event) => {
+        await self.selectScreen(event.options.screenId)
+      },
+    },
+
+    routeInputToPVWLayer: {
+      name: 'Route INPUT → PVW Layer (selected screen)',
+      options: [
+        {
+          type: 'dropdown',
+          id: 'layerId',
+          label: 'PVW Layer',
+          choices: () => self.getPVWLayers().map((l) => ({ id: l.id, label: l.name })),
+        },
+        {
+          type: 'dropdown',
+          id: 'inputId',
+          label: 'Input',
+          choices: () => self.getInputs().map((i) => ({ id: i.id, label: i.name })),
+        },
+      ],
+      callback: async (event) => {
+        const { layerId, inputId } = event.options
+        await self.routeInputToPVWLayer(layerId, inputId)
+      },
+    },
+
+    refreshState: {
+      name: 'Force refresh state',
+      callback: async () => {
+        await self.fullRefresh()
+      },
+    },
+  }
 }
